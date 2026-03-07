7ab355c42bfe821a3ae9500f278dcb3e0aec8f3c
Shinya <149946330+Inphelyse@users.noreply.github.com>
2026-03-07 16:10:49 +0800

    Add FindDuplicate program in C
    Implement a program to find and print duplicate elements in an integer array.

---
+++ 1D-Array/FindDuplicate.c
@@ -0,0 +1,43 @@
+/* Find Duplicate Elements in Array
+by CodeChum Admin
+
+Write a program that takes an input array of integers and prints a new array containing all the duplicate elements in the original array. */
+
+
+#include<stdio.h>
+void findDup(int size, int arr[size]){
+    int dup = 0;
+    for(int i = 0; i < size; i++){
+        for(int j = i + 1; j < size; j++){
+            if(arr[i] == arr[j]){
+                dup = arr[i];
+            }
+        }
+    }
+    
+    if(dup != 0){
+        printf("Duplicate elements in the array are: %d", dup);
+    } else {
+        printf("Duplicate elements in the array are: None");
+    }
+    
+    return;
+}
+
+int main(){
+    int size;
+    
+    printf("Enter the number of elements in the array: ");
+    scanf("%d", &size);
+    
+    int arr[size];
+    
+    printf("Enter %d elements in the array:\n", size);
+    for(int i = 0; i < size; i++){
+        scanf("%d", &arr[i]);
+    }
+    findDup(size, arr);
+
+    return 0;
+    
+}
