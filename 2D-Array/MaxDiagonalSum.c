16227d971002494380c1b108e60676fc597dcefc
Shinya <149946330+Inphelyse@users.noreply.github.com>
2026-03-07 21:12:01 +0800

    Calculate diagonal sum directly in main function
    Refactor diagonal sum calculation to be inline in main function.

--- 2D-Array/MaxDiagonalSum.c
+++ 2D-Array/MaxDiagonalSum.c
@@ -5,21 +5,15 @@ by CodeChum Admin
 Given a square matrix of integers, write a function to calculate the sum of its diagonal elements. The diagonal of a matrix refers to the set of elements that lie on the diagonal line that extends from the top-left corner of the matrix to the bottom-right corner. */
 
 #include<stdio.h>
-int sumDiag(int size, int arr[][size]){
-    int sum = arr[0][0];
-    for(int i = 1; i < size; i++){
-        sum += arr[i][i];
-    }
-    return sum;
-}
-
 int main(){
     int size;
     
-    printf("Enter the size of the matrix: ");
+    printf("Enter size of the matrix: ");
     scanf("%d", &size);
     
     int arr[size][size];
+    int sum = 0;
+
     printf("Enter the elements of the matrix:\n");
     for(int i = 0; i < size; i++){
         for(int j = 0; j < size; j++){
@@ -27,7 +21,17 @@ int main(){
         }
     }
     
-    int res = sumDiag(size, arr);
-    printf("The diagonal sum of the matrix is: %d", res);
+    sum = arr[0][0];
+    
+    printf("Diagonal sum of the matrix: ");
+    for(int i = 0; i < size; i++){
+        for(int j = 0; j < size; j++){
+            if(i == j){
+                sum += arr[i][j];
+            }
+        }
+    }
+    
+    printf("%d", sum);
     return 0;
 }
